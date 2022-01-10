c16edf14a26e2a218ace1852e6c6d76e7bdd3629
Vibhas <vibhass@khanlabschool.org>
2022-01-10 10:34:21 -0800

    jan 10 2

--- prompt.js
+++ prompt.js
@@ -1,5 +1,16 @@
 let prompt = require('prompt-sync')();
 
-let userInput = prompt("input some text");
+const answer = prompt ('Do you like JS? (Y/N)')
 
-console.log("the inputted text " + userInput);
+
+if (answer == "y" || answer == "Y") {
+    console.log("Nice! That's great!");
+
+} else if (answer == "n" || answer == "N") {
+    console.log("Aww Man");
+
+} else if (answer == "") {
+    console.log("At least say something!");
+} else {
+    console.log("I'm not sure if that's a yes or no.");
+}
