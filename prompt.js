be0a1ef67a0e8acf14b54bbc6c8f46f082344dbd
Vibhas <vibhass@khanlabschool.org>
2022-01-10 10:52:58 -0800

    end of class

--- prompt.js
+++ prompt.js
@@ -3,14 +3,30 @@ let prompt = require('prompt-sync')();
 const answer = prompt ('Do you like JS? (Y/N)')
 
 
-if (answer == "y" || answer == "Y") {
-    console.log("Nice! That's great!");
+// if (answer == "y" || answer == "Y") {
+//     console.log("Nice! That's great!");
 
-} else if (answer == "n" || answer == "N") {
-    console.log("Aww Man");
+// } else if (answer == "n" || answer == "N") {
+//     console.log("Aww Man");
 
-} else if (answer == "") {
-    console.log("At least say something!");
-} else {
-    console.log("I'm not sure if that's a yes or no.");
+// } else if (answer == "") {
+//     console.log("At least say something!");
+// } else {
+//     console.log("I'm not sure if that's a yes or no.");
+// }
+switch (answer) {
+    case "y":
+    case "Y":
+        console.log ("Nice that's Great!");
+        break;
+    case "n":
+    case "N":
+        console.log ("Aww Man");
+        break;
+    case "":
+        console.log("At least say something");
+        break;
+    default:
+        console.log ("Idk what that means");
+        break;
 }
