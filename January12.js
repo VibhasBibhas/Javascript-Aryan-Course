ef26949ace364732366bca3206ce2f200d3d791c
Vibhas <vibhass@khanlabschool.org>
2022-01-12 10:35:39 -0800

    idk

--- January12.js
+++ January12.js
@@ -4,8 +4,9 @@ function myFunc(johnnyboy){
     return output;
 }
 myFunc("johnny is bad")
-// const rand = Math.random()
-// const min = 0
-// const max = 10000000000000
-// const ourRandomNum = Math.floor(rand * (max - min)) + min
-// console.log(ourRandomNum)
+const rand = Math.random()
+const min = 0
+const max = 10000000000000
+const ourRandomNum = Math.floor(rand * (max - min)) + min
+console.log(ourRandomNum)
+console.log(myFunc(0, 2000000000))
