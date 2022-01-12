edb5430c9a76482b9392fe36fc1a2c24d57a3bcf
Vibhas <vibhass@khanlabschool.org>
2022-01-12 10:21:00 -0800

    idk

--- January 12.js
+++ January 12.js
@@ -1,6 +1,9 @@
 console.log(math.abs(input))
-
 console.log(Math.abs(prompt("neg number:")))
 const current = new Date();
 console.log(current.getHours());
 console.log(Date.now());
+const day1 = new Date(2020, 0)
+const day2 = new Date(2021, 0)
+
+console.log(day2 - day1)
