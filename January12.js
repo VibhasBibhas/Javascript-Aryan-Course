8c5e5ec699548fee9f12ccbc8678844f64db6249
Vibhas <vibhass@khanlabschool.org>
2022-01-12 10:29:20 -0800

    idk

--- January12.js
+++ January12.js
@@ -1,5 +1,5 @@
 const rand = Math.random()
 const min = 0
-const max = 1000000000000000000000000000000000000000000000000000000000000000
+const max = 10
 const ourRandomNum = Math.floor(rand + (max - min)) + min
 console.log(ourRandomNum)
