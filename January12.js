072021bfb878cce3f554eee6db97dcc281b6f3bf
Vibhas <vibhass@khanlabschool.org>
2022-01-19 10:15:28 -0800

    good mourn

--- January12.js
+++ January12.js
@@ -1,11 +1,3 @@
-function myFunc(min, max){
-    
-    const output = Math.floor(Math.random() * (max - min)) + min;
-    return output;
-}
-// const rand = Math.random()
-// const min = 0
-// const max = 10000000000000
-// const ourRandomNum = Math.floor(rand * (max - min)) + min
-// console.log(ourRandomNum)
-console.log(myFunc(0, 2000000000))
+
+let prompt = require('Type two numbers that you want to multiply, divide, add, or subtract')
+
