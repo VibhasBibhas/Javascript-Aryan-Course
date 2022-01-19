ec0b4ee45ce5b035082a1399646aaf9fe927d8d2
Vibhas <vibhass@khanlabschool.org>
2022-01-19 10:30:11 -0800

    main

--- January12.js
+++ January12.js
@@ -1,5 +1,25 @@
 
 let prompt = require('prompt-sync')();
 
-const answer = prompt ('Type numbers that you want to Add, Subtract, Multiply, or Divide')
+const number1 = prompt ('What is your first number?')
+
+const number2 = prompt ('What is your operation? (+,-,*,/)')
+
+const number3 = prompt ('What is your second number?')
+
+switch(number2) {
+    case "+":
+        console.log("addition");
+        break;
+    case "-":
+        console.log("subtraction");
+        break;
+    case "*":
+        console.log("multiplication")
+        break;
+    case "/":
+        console.log("division");
+        break;     
+}
+
 
