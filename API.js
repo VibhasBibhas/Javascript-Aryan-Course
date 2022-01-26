17ecd09212931c0758f88aaf1e97cbad8f68c961
Vibhas <vibhass@khanlabschool.org>
2022-01-26 10:22:06 -0800

    Luke change working

--- API.js
+++ API.js
@@ -1,15 +1,23 @@
-const https = require('https');
+const axios = require('axios').default;
+
+async function getLuke(){
+    let getLuke = await axios.get('https://swapi.dev/api/people/10');
+    let data = getLuke.data
+    console.log(data);
+}
+
+getLuke()
 
 
+// https.get("https://swapi.dev/api/films/6/", {}, (response) =>{
+//     let data = '';
+//     response.on('data', (chunk) => {
+//         data += chunk;
+//     });
+//     response.on('end', (_) => {
+//         console.log(JSON.parse(data));
+        
 
-https.get("https://api.kanye.rest/", {}, (response) =>{
-    let data = '';
-    response.on('data', (chunk) => {
-        data += chunk;
-    });
-    response.on('end', (_) => {
-        console.log(JSON.parse(data));
+//     });
 
-    });
-
-});
+// });
